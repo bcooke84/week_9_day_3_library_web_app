bbfadd1ac20c67b1f3677c1b5d1c9a81461fe0f7
Brian Cooke <brian.cooke@live.co.uk>
2018-05-23 13:40:59 +0100

    Book class created with properties, empty constructor, constructor & getters / setters

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,4 +1,43 @@
 package models;
 
+import java.util.HashSet;
+
 public class Book {
+
+    private String title;
+    private Library library;
+    private Borrower borrower;
+
+    public Book() {
+    }
+
+    public Book(String title, Library library, Borrower borrower) {
+        this.title = title;
+        this.library = library;
+        this.borrower = borrower;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+
+    public Borrower getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Borrower borrower) {
+        this.borrower = borrower;
+    }
 }
